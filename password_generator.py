0b98ad9b622b3f620213b325b2b89de81a95bf34
Manuel Parra <manuel.parra1@gmail.com>
2023-12-13 20:18:30 -0600

    variable rename

--- password_generator.py
+++ password_generator.py
@@ -6,14 +6,14 @@ digits = string.digits
 #special_characters = string.punctuation
 special_characters = "!@#$%&"
 
-alphabet = letters + digits + special_characters
+password_alphabet = letters + digits + special_characters
 
 password_length = 12
 
 password = ""
 
 for i in range(password_length):
-    password += "".join(secrets.choice(alphabet))
+    password += "".join(secrets.choice(password_alphabet))
 
 # Insert hyphens every 3 characters
 password = '-'.join([password[i:i+3] for i in range(0, len(password), 3)])
